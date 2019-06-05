56bb6f63c1ead83da0d5668597cbc50b3f6ba190
mallyk <kyle.mally@gmail.com>
2019-06-04 20:04:26 -0500

    sessions, permissions, login, and logout

---
+++ 032-sign-up/session.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func cleanSessions() {
+	fmt.Println("BEFORE CLEAN")
+	showSessions()
+	for k, v := range dbSessions {
+		if time.Now().Sub(v.lastActivity) > (time.Second * 30) {
+			delete(dbSessions, k)
+		}
+	}
+	dbSessionsCleaned = time.Now()
+	fmt.Println("AFTER CLEAN")
+	showSessions()
+}
+
+func showSessions() {
+	fmt.Println("********")
+	for k, v := range dbSessions {
+		fmt.Println(k, v.un)
+	}
+	fmt.Println("")
+}
